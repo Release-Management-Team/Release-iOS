602c3816e12ae625bfc92bdf27b3f84b0b8f664c
shinzz410 <tlswldnjs410@gmail.com>
2024-08-13 01:52:37 +0900

    Create TabView File

---
+++ Release-iOS/Release-iOS/Presentation/ActivityView.swift
@@ -0,0 +1,16 @@
+//
+//  ActivityView.swift
+//  Release-iOS
+//
+//  Created by 신지원 on 8/13/24.
+//
+
+import SwiftUI
+
+struct ActivityView: View {
+    var body: some View {
+        Text("This is the ActivityView")
+            .font(.title)
+            .padding()
+    }
+}
---
+++ Release-iOS/Release-iOS/Presentation/HomeView.swift
@@ -0,0 +1,16 @@
+//
+//  HomeView.swift
+//  Release-iOS
+//
+//  Created by 신지원 on 8/13/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("This is the Home View")
+            .font(.title)
+            .padding()
+    }
+}
