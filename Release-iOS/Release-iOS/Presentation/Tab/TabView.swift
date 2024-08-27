cf559307cd9c1f23c6ec9601da3f4314e907e856
shinzz410 <tlswldnjs410@gmail.com>
2024-08-28 07:37:03 +0900

    Separate TabView

---
+++ Release-iOS/Release-iOS/Presentation/Tab/TabView.swift
@@ -0,0 +1,49 @@
+//
+//  TabView.swift
+//  Release-iOS
+//
+//  Created by 신지원 on 8/28/24.
+//
+
+import SwiftUI
+
+class TabBarState: ObservableObject {
+    @Published var isTabBarHidden: Bool = false
+}
+
+struct TabView: View {
+    
+    @State private var selectedTab: Tab = .home
+    @StateObject private var tabBarState = TabBarState()
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            Spacer()
+            
+            switch selectedTab {
+            case .home:
+                HomeView()
+                    .padding(.bottom, tabBarState.isTabBarHidden ? 0 : 114+10)
+            case .activity:
+                ActivityView()
+            case .book:
+                BookView()
+                    .padding(.bottom, tabBarState.isTabBarHidden ? 0 : 114+10)
+            case .my:
+                MyView(my: my1)
+                    .padding(.bottom, tabBarState.isTabBarHidden ? 0 : 114+10)
+            }
+            
+            Spacer()
+            
+            if !tabBarState.isTabBarHidden {
+                CustomTabView(selectedTab: $selectedTab)
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color.black1)
+        .environmentObject(tabBarState)
+        .navigationBarHidden(true)
+        .toolbar(.hidden, for: .tabBar)
+    }
+}
--- Release-iOS/Release-iOS/Release_iOSApp.swift
+++ Release-iOS/Release-iOS/Release_iOSApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Release_iOSApp: App {
     var body: some Scene {
         WindowGroup {
-            LoginView()
+            ContentView()
         }
     }
 }
